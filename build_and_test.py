34cc691ec9ceb17931db8f28e843efa81c841ea2
Micah Ferrill <mcferrill@gmail.com>
2020-11-30 12:05:51 -0600

    Added simplistic stat tool from handbook samples.

--- build_and_test.py
+++ build_and_test.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
+from os.path import dirname, splitext, basename
+import glob
 import os
 import sys
 
+
+# Compile docopt usage to C
+docopt_path = f'{dirname(__file__)}/src/docopt/docopt.c/docopt_c.py'
+for filename in glob.glob('src/docopt/*.docopt'):
+    name, _ = splitext(basename(filename))
+    os.system(f'{sys.executable} {docopt_path} {filename} -o src/{name}_args.c')
+
+# Clear build directory before building
 if 'rebuild' in sys.argv:
     if sys.platform == 'win32':
         os.system('del /F /Q build')
     else:
         os.system('rm -rf build')
 
-os.system('mkdir build')
+# Create build directory
+if not os.path.exists('build'):
+    os.system('mkdir build')
 os.chdir('build')
+
+# Run cmake and whatever os-specific build tool.
 os.system('cmake ..')
 if sys.platform == 'win32':
     os.system('cmake --build . --config Release')
@@ -19,4 +33,4 @@ if sys.platform == 'win32':
 else:
     os.system('make')
     os.chdir('..')
-    os.system('./build/i106stat')
+    # os.system('./build/i106stat')
--- src/i106stat.c
+++ src/i106stat.c
@@ -1,7 +1,111 @@
 
 #include <stdio.h>
 
+#include "irig106ch10.h"
+#include "i106stat_args.c"
 
-int main(){
-    printf("Hello world!\n");
+
+typedef struct {
+    unsigned int type;
+    int id;
+    int packets;
+} ChanSpec;
+
+
+int main(int argc, char *argv[]){
+
+    // Get commandline args
+    DocoptArgs args = docopt(argc, argv, 1, "1");
+    if (args.help){
+        printf(args.help_message);
+        return 0;
+    }
+    else if (argc < 2){
+        printf(args.usage_pattern);
+        printf("\n");
+        return 0;
+    }
+
+    // Channel info
+	I106C10Header header;
+    int packets = 0, input_handle;
+    float byte_size = 0.0;
+    static ChanSpec * channels[0x10000];
+
+    // Open the source file.
+    I106Status status = I106C10Open(&input_handle, argv[1], READ);
+    if (status != I106_OK){
+        char msg[200] = "Error opening file ";
+        strcat(msg, argv[1]);
+        printf("%s", msg);
+        return 1;
+    }
+
+    // Iterate over selected packets (based on args).
+    while (1){
+
+        // Exit once file ends.
+        if ((status = I106C10ReadNextHeader(input_handle, &header)))
+            break;
+
+        // Increment overall size and packet counts.
+        byte_size += header.PacketLength;
+        packets++;
+
+        // Find the channel info based on ID and type.
+        int i;
+        for (i = 0; i < 0x10000; i++){
+
+            // Create a listing if none exists.
+            if (channels[i] == NULL){
+                channels[i] = (ChanSpec *)malloc(sizeof(ChanSpec));
+                memset(channels[i], 0, sizeof(ChanSpec));
+                channels[i]->id = (unsigned int)header.ChannelID;
+                channels[i]->type = (unsigned int)header.DataType;
+                channels[i]->packets = 0;
+            }
+
+            // 
+            if (channels[i]->id == (unsigned int)header.ChannelID && channels[i]->type == (unsigned int)header.DataType){
+                break;
+            }
+        }
+
+        channels[i]->packets++;
+    }
+
+	// Print details for each channel.
+    printf("Channel ID      Data Type%35sPackets\n", "");
+    printf("--------------------------------------------------------------------------------\n");
+    int i = 0;
+    while (channels[i] != NULL){
+        printf("Channel%3d", channels[i]->id);
+        printf("%6s", "");
+        printf("0x%-34x", channels[i]->type);
+        printf("%7d packets", channels[i]->packets);
+        printf("\n");
+        i++;
+    }
+
+    // Find a more readable size unit than bytes.
+    char *unit = "b";
+    if (byte_size > 1024){
+        byte_size /= 1024;
+        unit = "kb";
+    }
+    if (byte_size > 1024){
+        byte_size /= 1024;
+        unit = "mb";
+    }
+    if (byte_size > 1024){
+        byte_size /= 1024;
+        unit = "gb";
+    }
+
+    // Print file summary.
+    printf("--------------------------------------------------------------------------------\n");
+    printf("Summary for %s:\n", argv[1]);
+    printf("    Size: %.*f%s\n", 2, byte_size, unit);
+    printf("    Packets: %d\n", packets);
+    printf("    Channels: %d\n", i);
 }
