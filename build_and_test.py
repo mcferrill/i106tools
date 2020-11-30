b428ad83b796a8af355a7b066bbd5224851f396e
Micah Ferrill <mcferrill@gmail.com>
2020-11-30 08:41:39 -0600

    Initial commit.

---
+++ build_and_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+if 'rebuild' in sys.argv:
+    if sys.platform == 'win32':
+        os.system('del /F /Q build')
+    else:
+        os.system('rm -rf build')
+
+os.system('mkdir build')
+os.chdir('build')
+os.system('cmake ..')
+if sys.platform == 'win32':
+    os.system('cmake --build . --config Release')
+    os.chdir('..')
+    # os.system('.\\build\\Release\\test_runner.exe')
+else:
+    os.system('make')
+    os.chdir('..')
+    os.system('./build/i106stat')
