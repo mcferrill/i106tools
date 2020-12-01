3811ef8607a6042da5b4fe05a99fe3e31d43989d
Micah Ferrill <mcferrill@gmail.com>
2020-12-01 13:52:07 -0600

    Made output print channels in numerical order.

--- src/i106stat.c
+++ src/i106stat.c
@@ -5,9 +5,11 @@
 #include "i106stat_args.c"
 
 
+const int MAX_CHANNELS = 100;
+
 typedef struct {
-    unsigned int type;
     int id;
+    unsigned int type;
     int packets;
 } ChanSpec;
 
@@ -17,12 +19,11 @@ int main(int argc, char *argv[]){
     // Get commandline args
     DocoptArgs args = docopt(argc, argv, 1, "1");
     if (args.help){
-        printf(args.help_message);
+        printf("%s", args.help_message);
         return 0;
     }
     else if (argc < 2){
-        printf(args.usage_pattern);
-        printf("\n");
+        printf("%s\n", args.usage_pattern);
         return 0;
     }
 
@@ -30,7 +31,7 @@ int main(int argc, char *argv[]){
 	I106C10Header header;
     int packets = 0, input_handle;
     float byte_size = 0.0;
-    static ChanSpec * channels[0x10000];
+    static ChanSpec * channels[0x10000][255];
 
     // Open the source file.
     I106Status status = I106C10Open(&input_handle, argv[1], READ);
@@ -52,60 +53,47 @@ int main(int argc, char *argv[]){
         byte_size += header.PacketLength;
         packets++;
 
-        // Find the channel info based on ID and type.
-        int i;
-        for (i = 0; i < 0x10000; i++){
-
-            // Create a listing if none exists.
-            if (channels[i] == NULL){
-                channels[i] = (ChanSpec *)malloc(sizeof(ChanSpec));
-                memset(channels[i], 0, sizeof(ChanSpec));
-                channels[i]->id = (unsigned int)header.ChannelID;
-                channels[i]->type = (unsigned int)header.DataType;
-                channels[i]->packets = 0;
-            }
-
-            // 
-            if (channels[i]->id == (unsigned int)header.ChannelID && channels[i]->type == (unsigned int)header.DataType){
-                break;
-            }
+        // Create a listing if none exists.
+        if (channels[header.ChannelID][header.DataType] == NULL){
+            ChanSpec channel = {header.ChannelID, header.DataType, 1};
+            channels[header.ChannelID][header.DataType] = malloc(sizeof(ChanSpec));
+            memcpy(channels[header.ChannelID][header.DataType], &channel, sizeof(ChanSpec));
         }
 
-        channels[i]->packets++;
+        // Increment packet count if not
+        else
+            channels[header.ChannelID][header.DataType]->packets++;
     }
 
 	// Print details for each channel.
     printf("Channel ID      Data Type%35sPackets\n", "");
     printf("--------------------------------------------------------------------------------\n");
-    int i = 0;
-    while (channels[i] != NULL){
-        printf("Channel%3d", channels[i]->id);
-        printf("%6s", "");
-        printf("0x%-34x", channels[i]->type);
-        printf("%7d packets", channels[i]->packets);
-        printf("\n");
-        i++;
+    int channel_count = 0;
+    for (int i=0;i<MAX_CHANNELS;i++){
+        for (int j=0;j<=255;j++){
+            if (channels[i][j] == NULL)
+                continue;
+            channel_count += 1;
+            printf("Channel%3d", channels[i][j]->id);
+            printf("%6s", "");
+            printf("0x%-34x", channels[i][j]->type);
+            printf("%7d packets", channels[i][j]->packets);
+            printf("\n");
+        }
     }
 
     // Find a more readable size unit than bytes.
-    char *unit = "b";
-    if (byte_size > 1024){
+    char units[][3] = {"b", "kb", "mb", "gb", "tb"};
+    int unit = 0;
+    while (byte_size > 1024){
+        unit++;
         byte_size /= 1024;
-        unit = "kb";
-    }
-    if (byte_size > 1024){
-        byte_size /= 1024;
-        unit = "mb";
-    }
-    if (byte_size > 1024){
-        byte_size /= 1024;
-        unit = "gb";
     }
 
     // Print file summary.
     printf("--------------------------------------------------------------------------------\n");
     printf("Summary for %s:\n", argv[1]);
-    printf("    Size: %.*f%s\n", 2, byte_size, unit);
+    printf("    Size: %.*f%s\n", 2, byte_size, units[unit]);
     printf("    Packets: %d\n", packets);
-    printf("    Channels: %d\n", i);
+    printf("    Channels: %d\n", channel_count);
 }
